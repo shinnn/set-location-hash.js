c81e491ee1717337f91db7cc521d2129e40e9ce9
shinnn <snnskwtnb@gmail.com>
2014-04-29 14:43:32 +0900

    improve readability of test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var setLocationHash = require('./dist/set-location-hash.js');
 
 var body = document.body;
-
+body.style.padding = '0 1px 1px 0';
 body.style.height = '9999px';
 
 // create 'div#some' element
@@ -12,70 +12,67 @@ target.style.position = 'absolute';
 target.style.top = '5000px';
 body.appendChild(target);
 
+var historyAvailable = typeof history.pushState === 'function' &&
+                       typeof history.replaceState === 'function';
+
 window.scrollTo(0, 0);
 
-test('if hash changed', function(t) {
+test('setLocationHash()', function(t) {
   'use strict';
+  if (historyAvailable) {
+    t.plan(6);
+  } else {
+    t.plan(4);
+  }
 
   setLocationHash('some');
   
-  t.plan(1);
-  t.equal(location.hash, '#some');
-});
-
-test('if scroll position unchanged', function(t) {
-  'use strict';
+  t.equal(
+    location.hash,
+    '#some',
+    'should change fragment identifier of URL.'
+  );
 
   setLocationHash('some');
-
-  t.plan(1);
+  
   t.equal(
     body.scrollTop || document.documentElement.scrollTop,
-    0
+    0,
+    'should not change scroll position.'
   );
-});
 
-test('if returns current URL', function(t) {
-  'use strict';
-
-  var returnVal = setLocationHash('some');
-
-  t.plan(1);
-  t.equal(returnVal, location.href);
-});
-
-test('if empty hash available', function(t) {
-  'use strict';
+  t.equal(
+    setLocationHash('some'),
+    location.href,
+    'should return current URL.'
+  );
 
   setLocationHash('');
 
-  t.plan(1);
-  t.equal(location.href.split('#')[1], '');
-});
-
-if (typeof history.pushState === 'function' &&
-    typeof history.replaceState === 'function') {
-
-  test('replace option', function(t) {
-    'use strict';
-
+  t.equal(
+    location.href.split('#')[1],
+    '',
+    'should remove fragment identifier when the argument is empty string.'
+  );
+  
+  if (historyAvailable) {
     var currentHistoryLength = history.length;
-
     setLocationHash('baz', {replace: true});
+    
+    t.equal(
+      history.length,
+      currentHistoryLength,
+      'should not push new history when "replace" option enabled.'
+    );
 
-    t.plan(1);
-    t.equal(history.length, currentHistoryLength);
-  });
-
-  test('force option', function(t) {
-    'use strict';
-
-    var currentHistoryLength = history.length;
+    currentHistoryLength = history.length;
     var currentFrag = location.hash.substring(1);
-
     setLocationHash(currentFrag, {force: true});
-
-    t.plan(1);
-    t.equal(history.length, currentHistoryLength + 1);
-  });
-}
+    
+    t.equal(
+      history.length,
+      currentHistoryLength + 1,
+      'should push new history anyway when "force" option enabled.'
+    );
+  }
+});
